a2ac562eaab47994f8bcf2f26928254f349187bb
riasat <11375716+crimsonvspurple@users.noreply.github.com>
2022-12-10 01:06:52 +0600

    frontend app source filter consts

---
+++ commons/src/main/java/red/eminence/commons/meta/App.java
@@ -0,0 +1,7 @@
+package red.eminence.commons.meta;
+
+public enum App
+{
+    falguni,    // Spring-tan
+    hoimonti    // Autumn-tan
+}
---
+++ commons/src/main/java/red/eminence/commons/meta/Platform.java
@@ -0,0 +1,13 @@
+package red.eminence.commons.meta;
+
+public enum Platform
+{
+    web,
+    android,
+    ios,
+    fuchsia,
+    linux,
+    macos,
+    windows,
+    unknown
+}
